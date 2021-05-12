64ec9061f6059877a7dd867d45f40ff09d6fbd6d
Eva Boneš <eb1690@hpc-login1.arnes.si>
2021-05-12 22:35:18 +0200

    started fine-tuning

--- fairseq_extension/bash scripts/binarize_ted_en-sl.sh
+++ fairseq_extension/bash scripts/binarize_ted_en-sl.sh
@@ -3,9 +3,12 @@ cd ..
 
 DATA=data/datasets-ted
 BIN=data/data-binarized-ted/datasets_en-sl
+TDICT=data/data-binarized/datasets_en-sl/dict.sl.txt
+SDICT=data/data-binarized/datasets_en-sl/dict.en.txt
 
-python fairseq_cli/preprocess.py \
+python3 fairseq_cli/preprocess.py \
   --source-lang en --target-lang sl \
   --trainpref $DATA/train --validpref $DATA/valid \
+  --tgtdict $TDICT --srcdict $SDICT \
   --destdir $BIN --thresholdtgt 0 --thresholdsrc 0 \
   --workers 20
