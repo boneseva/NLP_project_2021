38837e73c551cdc27db902c1d3af36e61df9f7ab
es6317 <erika.five123@gmail.com>
2021-05-16 20:54:18 +0200

    evaluation code

---
+++ clean_trained.py
@@ -0,0 +1,71 @@
+
+def clean_row(row):
+    return row.replace("&apos;", "'")\
+        .replace("@-@", "-")\
+        .replace("&quot;", '"')\
+        .replace(" ,", ",")\
+        .replace(" .", ".")\
+        .replace(" ?", "?")\
+        .replace(" !", "!")
+
+def parse_file(dataset, results_folder, model):
+    try:
+        if dataset == 'asistent':
+            file_to_clean = results_folder + "generate-train.txt"
+        else:
+            file_to_clean = results_folder + "generate-valid.txt"
+        clean_file_valid = results_folder + "valid.sl"
+        clean_file_translation = results_folder + "translated-" + model + ".en-sl.sl"
+
+        print("Parsing", file_to_clean)
+
+        current = None
+        ordered_rows = {}
+        with open(file_to_clean, 'r', encoding='utf-8') as f, \
+                open(clean_file_valid, 'w', encoding='utf-8') as f_valid, \
+                open(clean_file_translation, 'w', encoding='utf-8') as f_translation:
+            for row in f:
+                if (current is None or current == "H") and row.startswith("S-"):
+                    current = "S"
+                elif current == "S" and row.startswith("T-"):
+                    replaced = clean_row(row)
+                    index = int(replaced.split("\t")[0].split("-")[1])
+                    ordered_rows[index] = {}
+                    ordered_rows[index]['valid'] = " ".join(replaced.split("\t")[1:])
+                    f_valid.write(" ".join(replaced.split("\t")[1:]))
+                    current = "T"
+                elif current == "T" and row.startswith("H-"):
+                    replaced = clean_row(row)
+                    index = int(replaced.split("\t")[0].split("-")[1])
+                    ordered_rows[index]['translated'] = " ".join((" ".join(replaced.split("\t")[1:])).split(" ")[1:])
+                    f_translation.write(" ".join((" ".join(replaced.split("\t")[1:])).split(" ")[1:]))
+                    current = "H"
+
+        if dataset == 'ted':
+            clean_file_first60 = results_folder + "translated-" + model + "-for-manual-evaluation.en-sl.sl"
+            with open(clean_file_first60, "w", encoding='utf-8') as f:
+                for index in sorted(list(ordered_rows.keys())):
+                    f.write(ordered_rows[index]['translated'])
+    except Exception:
+        print("Couldn't clean file", file_to_clean)
+
+def check_if_row_needed(needed_rows, row):
+    for i, nrow in enumerate(needed_rows):
+        if row[:10] in nrow:
+            return i
+    return -1
+
+if __name__ == '__main__':
+    for dataset in ['general', 'asistent', 'ted']:
+
+        base_folder = "fairseq_extension/data/translated-" + dataset + "/"
+
+        for i in range(1, 9):
+            results_folder = base_folder + "results" + str(i) + "/"
+            parse_file(dataset, results_folder, "general")
+
+        if dataset != 'general':
+            # there is no ted_conf evaluation
+            for i in range(1, 4):
+                results_folder = base_folder + "results_ted_conf" + str(i) + "/"
+                parse_file(dataset, results_folder, "ted")
--- evaluate.py
+++ evaluate.py
@@ -1,35 +1,81 @@
 import nltk
+from jiwer import wer
+import sys
 
-#evaluation_data_prefix = 'fairseq_extension/data/translated-ted/valid'
-#translation_prefix = "fairseq_extension/data/translated-ted/translated-pretrained"
-evaluation_data_prefix = 'fairseq_extension/data/asistent-testset/asistent_testset'
-translation_prefix = "fairseq_extension/data/asistent-testset/translated-pretrained"
+evaluation_data = {'ted': 'fairseq_extension/data/translated-ted/valid',
+         'asistent': 'fairseq_extension/data/asistent-testset/asistent_testset',
+         'general': 'fairseq_extension/data/translated-general/valid'}
 
-hypotheses = []
-references = []
-list_of_references = []
-joined_hypotheses = []
-joined_list_of_references = []
+base_translation_path = "fairseq_extension/data/translated-"
 
-for lang_pair in ['en-sl', 'sl-en']:
-    print('Evaluating', lang_pair)
-    with open(evaluation_data_prefix + '.' + lang_pair.split('-')[1], 'r', encoding='utf-8') as references_file:
-        with open(translation_prefix + '.' + lang_pair + '.' + lang_pair.split('-')[1], 'r', encoding='utf-8') as hypotheses_file:
+def evaluate_one(translation_path, valid_path):
+    hypotheses = []
+    references = []
+    list_of_references = []
+    joined_references = []
+    joined_hypotheses = []
+    joined_list_of_references = []
+
+    lang_pair = 'en-sl'
+    with open(valid_path + '.' + lang_pair.split('-')[1], 'r',
+              encoding='utf-8') as references_file:
+        with open(translation_path + '.' + lang_pair + '.' + lang_pair.split('-')[1], 'r',
+                  encoding='utf-8') as hypotheses_file:
             for r, h in zip(references_file, hypotheses_file):
-                list_of_references.append([r.split()])
-                references.append(r.split())
-                hypotheses.append(h.split())
-                joined_hypotheses.append(h)
-                joined_list_of_references.append([r])
+                list_of_references.append([r[:-1].split()])
+                references.append(r[:-1].split())
+                hypotheses.append(h[:-1].split())
+                joined_references.append(r[:-1])
+                joined_hypotheses.append(h[:-1])
+                joined_list_of_references.append([r[:-1]])
 
     print('BLEU:', nltk.translate.bleu_score.corpus_bleu(list_of_references, hypotheses))
     print("CHRF:", nltk.translate.chrf_score.corpus_chrf(references, hypotheses))
     print("GLEU:", nltk.translate.gleu_score.corpus_gleu(list_of_references, hypotheses))
 
-    meteor_score = sum([nltk.translate.meteor_score.meteor_score(r, jh) for r, jh in zip(joined_list_of_references, joined_hypotheses)]) / len(joined_hypotheses)
+    meteor_score = sum([nltk.translate.meteor_score.meteor_score(r, jh) for r, jh in
+                        zip(joined_list_of_references, joined_hypotheses)]) / len(joined_hypotheses)
     print("METEOR:", meteor_score)
     print("NIST:", nltk.translate.nist_score.corpus_nist(list_of_references, hypotheses))
     try:
         print("RIBES:", nltk.translate.ribes_score.corpus_ribes(list_of_references, hypotheses))
     except ZeroDivisionError:
         print("RIBES undefined")
+    print("WER:", wer(joined_references, joined_hypotheses))
+
+def evaluate_dataset(model, dataset):
+    print("Evaluating dataset", dataset)
+
+    translation_path = base_translation_path + dataset
+    if model == 'pretrained':
+        evaluate_one(translation_path + '/translated-pretrained', evaluation_data[dataset])
+    elif model == 'general':
+        for i in range(1, 9):
+            print("Evaluating epoch", i)
+            results_path = translation_path + '/results' + str(i)
+            evaluate_one(results_path + "/translated-general", results_path + "/valid")
+    elif model == 'domain':
+        for i in range(1, 4):
+            print("Evaluating configuration", i)
+            results_path = translation_path + '/results_ted_conf' + str(i)
+            evaluate_one(results_path + "/translated-ted", results_path + "/valid")
+
+
+
+if __name__ == '__main__':
+    model = sys.argv[1]
+    print("Evaluating model", model)
+    if len(sys.argv) > 2:
+        evaluate_dataset(sys.argv[2])
+    else:
+        if model == 'pretrained' or model == 'domain':
+            for dataset in ['asistent', 'ted']:
+                evaluate_dataset(model, dataset)
+        elif model == 'general':
+            for dataset in evaluation_data:
+                evaluate_dataset(model, dataset)
+
+
+
+
+
