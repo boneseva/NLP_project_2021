4c6db14731ae30d1fd7457373b40b213b170438c
es6317 <erika.five123@gmail.com>
2021-05-21 00:51:47 +0200

    download general binarized

--- scripts/download_general_binarized.sh
+++ scripts/download_general_binarized.sh
@@ -4,7 +4,7 @@ DIR=data/data-binarized
 mkdir -p $DIR
 cd $DIR
 
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1ochxd0Uk52VNWjOpbXMNJtxcG7gi1Dg8' -O general_binarized.zip
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1SvRui0Did2NL_ssSXBVzkhAAq7OokxQj' -O general_binarized.zip
 
 unzip general_binarized.zip
 rm general_binarized.zip
