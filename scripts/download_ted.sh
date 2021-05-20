cfa8c08d40a2df39f54cc56808500163867261c6
es6317 <erika.five123@gmail.com>
2021-05-20 23:07:21 +0200

    added ted download extras

--- scripts/download_ted.sh
+++ scripts/download_ted.sh
@@ -8,11 +8,10 @@ corpora="TED2013 TED2020"
 
 wget -O TED2013.zip -c https://opus.nlpl.eu/download.php?f=TED2013/v1.1/moses/en-sl.txt.zip
 wget -O TED2020.zip -c https://opus.nlpl.eu/download.php?f=TED2020/v1/moses/en-sl.txt.zip
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1hIODDj_TbyfDa0o5ouMmzh42ZSmYm7PD' -O extra.zip
 
 for corpus in $corpora
 do
-  mv ${corpus}.zip $DIR/${corpus}.zip
-  cd $DIR
 	unzip ${corpus}.zip
 	rm README
 	rm LICENSE
@@ -21,9 +20,13 @@ do
 	rm ${corpus}.zip
 	mv ${corpus}.en-sl.en ${corpus}.en
 	mv ${corpus}.en-sl.sl ${corpus}.sl
-	cd ../..
 done
+
+unzip extra.zip
+rm extra.zip
 
 #fix dataset file endings from drive
 for f in $DIR/*en.txt; do mv "$f" "${f%en.txt}.en"; done
 for f in $DIR/*sl.txt; do mv "$f" "${f%sl.txt}.sl"; done
+
+cd ../..
