9a46903e225b3db616811fd0f454a86ad5f61636
bounesh <eb1690@student.uni-lj.si>
2021-05-21 21:20:56 +0200

    readme

--- scripts/binarize_ted.sh
+++ scripts/binarize_ted.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATA=data/datasets-ted
-BIN=data/data-binarized-ted/datasets_en-sl
+BIN=data/data-binarized-ted
 TDICT=data/data-binarized/dict.sl.txt
 SDICT=data/data-binarized/dict.en.txt
 
--- scripts/download_general_binarized.sh
+++ scripts/download_general_binarized.sh
@@ -4,7 +4,7 @@ DIR=data/data-binarized
 mkdir -p $DIR
 cd $DIR
 
-wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1SvRui0Did2NL_ssSXBVzkhAAq7OokxQj' -O general_binarized.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1emS5aPX-pkJPm6NsgibfO3QLq8NNJHPr' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1emS5aPX-pkJPm6NsgibfO3QLq8NNJHPr" -O general_binarized.zip && rm -rf /tmp/cookies.txt
 
 unzip general_binarized.zip
 rm general_binarized.zip
