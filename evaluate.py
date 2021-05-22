b2b101d2cf2f10e739650818c12fbd2a27f49d19
Eva Boneš <eb1690@student.uni-lj.si>
2021-05-22 15:53:43 +0200

    Clean files a bit

--- evaluate.py
+++ evaluate.py
@@ -77,8 +77,4 @@ if __name__ == '__main__':
         elif model == 'general':
             for dataset in evaluation_data:
                 evaluate_dataset(model, dataset)
-
-
-
-
-
+                
